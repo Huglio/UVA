1ac58f081ac9452d11a078295e7e663b4f58f354
Huglio <gabrielhuglio@me.com>
2019-06-19 22:09:51 -0300

    12049

---
+++ 12049.cpp
@@ -0,0 +1,38 @@
+#include "pch.h"
+#include <stdio.h>
+#include <cmath>
+#include <map>
+#include <set>
+
+using namespace std;
+
+int main()
+{
+	int k; scanf("%d", &k);
+	while (k--)
+	{
+		int n, m; scanf("%d %d", &n, &m);
+
+		set<int> Nums;
+
+		map<int, int> A;
+		map<int, int> B;
+
+		for (int i = 0; i < n; i++)
+		{
+			int x; scanf("%d", &x);
+			A[x]++; Nums.insert(x);
+		}
+		for (int i = 0; i < m; i++)
+		{
+			int x; scanf("%d", &x);
+			B[x]++; Nums.insert(x);
+		}
+
+		int contador = 0;
+		for (set<int>::iterator it = Nums.begin(); it != Nums.end(); it++)
+			contador += abs(A[*it] - B[*it]);
+
+		printf("%d\n", contador);
+	}
+}
